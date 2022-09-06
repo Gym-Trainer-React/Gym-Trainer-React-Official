c5b450c22f70d765e8689fb8029c35f190141bf4
LeonardG1993 <leonardguilloryiv@gmail.com>
2022-09-06 14:18:16 -0500

    First commit, added WorkoutLog component

---
+++ gym-trainer/src/components/WorkoutLog.tsx
@@ -0,0 +1,22 @@
+
+
+export interface WorkoutLogProps{
+   userId: number;
+   workoutId: number;
+   comment: string;
+   completed: boolean;
+   timestamp: Date;
+}
+
+function WorkoutLog(){
+
+    return 
+}
+
+function createWorkoutLog(){
+
+}
+
+function displayWorkoutLog(){
+    
+}
